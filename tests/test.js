fb5107f2be64468e7e3276f84083d723ad6c310b
Itay Eshkar <itayeshkar@Itays-MacBook-Air-2.local>
2025-04-07 09:37:53 +0300

    basic tests are up and running

--- tests/test.js
+++ tests/test.js
@@ -5,25 +5,54 @@ const BASE_URL = 'http://localhost:3000';
 const TAGS = ['cardiology', 'oncology', 'radiology', 'internal medicine'];
 
 const generateItem = () => ({
-    title: faker.lorem.sentence(),
-    subtitle: faker.lorem.sentence(),
-    vettedDate: faker.date.past().toISOString().split('T')[0],
-    content: faker.lorem.paragraphs(2),
-    tags: faker.helpers.arrayElements(TAGS, Math.floor(Math.random() * 3) + 1)
+  title: faker.lorem.sentence(),
+  subtitle: faker.lorem.sentence(),
+  vettedDate: faker.date.past().toISOString().split('T')[0],
+  content: faker.lorem.paragraphs(2),
+  tags: faker.helpers.arrayElements(TAGS, Math.floor(Math.random() * 3) + 1)
 });
-  
-const seed = async () => {
-    console.log('Starting seeding...');
+
+const createItem = async () => {
+  const item = generateItem();
+  const response = await axios.post(`${BASE_URL}/api/items/`, item);
+  return response;
+}
+
+const updateItem = async (id) => {
+  const item = generateItem()
+  const response = await axios.put(`${BASE_URL}/api/items/id?id=${id}`, item);
+  return response;
+}
+
+const deleteItem = async (id) => {
+  const response = await axios.delete(`${BASE_URL}/api/items/id?id=${id}`);
+  return response;
+}
+
+const getAllItems = async (id) => {
+  const response = await axios.get(`${BASE_URL}/api/items/`)
+  return response;
+}
+
+describe('Item API Integration Tests', () => {
+  test('Should create 100 knowledge items successfully', async () => {
     for (let i = 0; i < 100; i++) {
-      const item = generateItem();
-      try {
-        const res = await axios.post(`${BASE_URL}/api/items/`, item);
-        console.log(`Inserted item ${i + 1}:\n`, res.data.data.id ?? '✔');
-      } catch (err) {
-        console.error(`Failed to insert item ${i + 1}:`, err.response?.data || err.message);
-      }
+      const response = await createItem();
+      expect(response.status).toBe(201);
+      expect(response.data.success).toBe(true);
+      expect(response.data.data).toHaveLength(1);
     }
-    console.log('Done seeding.');
-  };
-  
-seed();
+  }, 60_000);
+
+  test('Should delete id 3', async () => {
+    const response = await deleteItem(3);
+    expect(response.status).toBe(204);
+  }, 60_000);
+
+  test('Should update id 7', async () => {
+    const response = await updateItem(7);
+    expect(response.status).toBe(200);
+    expect(response.data.success).toBe(true);
+    expect(response.data.data).toHaveLength(1); 
+  }, 60_000);
+});
